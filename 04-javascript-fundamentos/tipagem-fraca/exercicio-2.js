202c669441f38991b116b5df5a0e4c4e99f770bf
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-31 22:41:31 -0300

    feat: exercicio 2 (alteração de var)2

--- 04-javascript-fundamentos/tipagem-fraca/exercicio-2.js
+++ 04-javascript-fundamentos/tipagem-fraca/exercicio-2.js
@@ -1,3 +1,7 @@
 /* Exercício 2: Crie uma variável com o nome "idade" e atribua um valor 
  * numérico. Em seguida, altere o valor da variável para uma string e imprima
  * a variável na tela.*/
+var idade2= 26;
+console.log(idade2);
+idade2= "Minha idade é 26";
+console.log(idade2);
