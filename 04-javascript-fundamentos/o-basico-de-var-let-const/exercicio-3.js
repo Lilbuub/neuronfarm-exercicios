bde74a95dd95324f6df5b76f49d317b9c58e7a2b
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-31 22:32:00 -0300

    feat: exercicio 3 (CONST)

---
+++ 04-javascript-fundamentos/o-basico-de-var-let-const/exercicio-3.js
@@ -0,0 +1,5 @@
+/* Exercício 3: Crie uma constante com o nome "idade" utilizando a 
+ * palavra-chave CONST e atribua seu valor de idade. Em seguida, 
+ * tente alterar o valor da constante e imprima a constante na tela.*/
+const idade= 26
+console.log(idade); //não dá para alterar uma constante.
