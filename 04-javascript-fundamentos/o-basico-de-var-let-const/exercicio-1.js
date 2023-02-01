f7abc6bbf4d098497c5d1ec7aea965132384d002
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-31 22:25:20 -0300

    feat: Exercicio 1 (VAR)

---
+++ 04-javascript-fundamentos/o-basico-de-var-let-const/exercicio-1.js
@@ -0,0 +1,5 @@
+/* Exercício 1: Crie uma variável com o nome "nome" utilizando a palavra-chave
+ * VAR e atribua seu nome completo como valor. Em seguida, imprima a variável 
+ * na tela.*/
+var nome= "thais"
+console.log(nome);
