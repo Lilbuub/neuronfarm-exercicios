c4c6e7702a0d0911b23701fb1e5fade294679bd4
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-31 22:53:41 -0300

    style: Formatação

--- 04-javascript-fundamentos/tipagem-fraca/exercicio-3.js
+++ 04-javascript-fundamentos/tipagem-fraca/exercicio-3.js
@@ -1,7 +1,7 @@
 /* Exercício 3: Crie uma variável com o nome "nome" e atribua uma string
  * como valor. Em seguida, altere o valor da variável para um número e imprima 
  * a variável na tela.*/
-var nome2= "Thais";
+var nome2 = "Thais";
 console.log(nome2);
-var nome2= 26;
+var nome2 = 26;
 console.log(nome2);
