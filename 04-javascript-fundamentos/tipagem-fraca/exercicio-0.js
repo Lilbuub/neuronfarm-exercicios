9e9c231705c797dc4d61e1f1fef6989027fad869
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-01-31 22:35:52 -0300

    feat: exercicio 0 (Tipagem Fraca)

---
+++ 04-javascript-fundamentos/tipagem-fraca/exercicio-0.js
@@ -0,0 +1,5 @@
+/* Tipagem Fraca 
+ * Exercício 0: Dê um exemplo de como a tipagem fraca afeta a programação 
+ * em JavaScript*/
+/* RESP: Nomeclatura das variaveis, comentários desnecessários que fazem 
+ * poluição do código, realizar boas praticas*/
