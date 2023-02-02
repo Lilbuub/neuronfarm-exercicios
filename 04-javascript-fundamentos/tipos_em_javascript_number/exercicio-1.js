4e073f42ab13c453167f847f1f3fc4f1519d9458
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-01 21:09:32 -0300

    feat: exercicio 1 (soma de numeros)

---
+++ 04-javascript-fundamentos/tipos_em_javascript_number/exercicio-1.js
@@ -0,0 +1,8 @@
+/*Exercício 1: Crie duas variáveis com o nome "a" e "b" e atribua valores numéricos. Em seguida, crie uma terceira variável com o nome "soma"
+ que receba a soma das duas variáveis anteriores e imprima o resultado na tela.*/
+ 
+ a = 3;
+ b = 6;
+ soma = a + b;
+ console.log(soma);
+ 
