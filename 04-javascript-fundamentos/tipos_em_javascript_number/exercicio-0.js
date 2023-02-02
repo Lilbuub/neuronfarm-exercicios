c1f4e9d6bc2c81990b1fd9bcb7e95a75ec5c65f3
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-01 21:06:07 -0300

    feat: exercicio 0 (number)

---
+++ 04-javascript-fundamentos/tipos_em_javascript_number/exercicio-0.js
@@ -0,0 +1,4 @@
+//Exercício 0: Como você pode garantir que um número não seja NaN (Not a Number)?
+//RESP: Garantindo que ambos os valores sejam números e não uma string, exemplo:
+console.log(4 * 5); //Correto
+console.log('Uhu' * 5); //Incorreto
