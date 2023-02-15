f3cf995fdcfd54391a34085570b365ff0e96762c
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-14 21:33:35 -0300

    feat: exericio 2 - math.round

---
+++ 04-javascript-fundamentos/number_alguns_cuidados/exercicio-2.js
@@ -0,0 +1,3 @@
+//Exercício 2: Crie uma variável com o nome "numero" e atribua um valor numérico. Em seguida, utilize a função Math.round() para arredondar o valor da variável e imprima o resultado na tela.
+
+//exercicio não feito, porque ainda não foi passado nos videos da udemy// pulei por enquanto
