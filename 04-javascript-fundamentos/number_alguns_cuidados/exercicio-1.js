8f0d2a7e5ab63b7dc0d11baebc6cb65ccfe16854
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-14 21:31:30 -0300

    feat: exercicio 01 de number

---
+++ 04-javascript-fundamentos/number_alguns_cuidados/exercicio-1.js
@@ -0,0 +1,16 @@
+/* Exercício 1: Crie uma variável com o nome "numero1" e atribua um valor numérico com ponto flutuante. Em seguida, 
+crie uma outra variável com o nome "numero2" e atribua outro valor numérico com ponto flutuante. Em seguida, calcule a 
+divisão entre as duas variáveis e imprima o resultado na tela.*/
+
+let numero1 = 5.3;
+let numero2 = 8.5;
+
+let soma = numero1/numero2;
+
+console.log(soma);
+
+
+
+
+
+
