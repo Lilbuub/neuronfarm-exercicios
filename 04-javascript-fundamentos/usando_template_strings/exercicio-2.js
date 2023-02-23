a0e5269beae61aec035f7c3149eb4f095630d317
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-23 17:21:11 -0300

    feat: exercicio2 usando template strings

---
+++ 04-javascript-fundamentos/usando_template_strings/exercicio-2.js
@@ -0,0 +1,6 @@
+/* Crie duas variveis com o nome "num1" e "num2" e atribua valores a elas. em seguida
+utilize uma template string para exibir a soma das duas variavéis na tela. */
+
+const num1 = 36;
+const num2 = 14;
+console.log(num1 + num2);
