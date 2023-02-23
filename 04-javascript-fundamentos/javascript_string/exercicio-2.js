87846eb30f4d750a86bf6ac626acbfa9c189ac43
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-23 17:16:10 -0300

    feat: exercicio2 javascript string

---
+++ 04-javascript-fundamentos/javascript_string/exercicio-2.js
@@ -0,0 +1,6 @@
+//Exercício 2: Crie uma variável com o nome "frase" e atribua uma string como valor. Em seguida, utilize a função toUpperCase para transformar a string em letras maiúsculas e imprima o resultado na tela.
+
+const frase = 'como está hoje?';
+const frase2 = frase.toUpperCase();
+console.log(frase2);
+
