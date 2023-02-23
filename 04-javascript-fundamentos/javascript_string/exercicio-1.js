0bb49604c7724b0ce94b50a7671f819e6dc418c7
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-23 17:08:00 -0300

    feat: exercicio1 javascript string

---
+++ 04-javascript-fundamentos/javascript_string/exercicio-1.js
@@ -0,0 +1,6 @@
+// Exercício 1: Crie uma variável com o nome "nome" e atribua uma string como valor. Em seguida, utilize a função length para verificar o tamanho da string e imprima o resultado na
+
+const nome = "Thais Giovanna"
+nome.length;
+console.log(nome.length);
+
