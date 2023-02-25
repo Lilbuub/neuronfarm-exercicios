6ce5aade757c72262c385d760f75fe575109d441
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-24 21:38:22 -0300

    refactor: correção de string

--- 04-javascript-fundamentos/javascript_string/exercicio-1.js
+++ 04-javascript-fundamentos/javascript_string/exercicio-1.js
@@ -1,6 +1,6 @@
 // Exercício 1: Crie uma variável com o nome "nome" e atribua uma string como valor. Em seguida, utilize a função length para verificar o tamanho da string e imprima o resultado na
 
 const nome = "Thais Giovanna"
-nome.length;
-console.log(nome.length);
+let tamanho = nome.length;
+console.log(tamanho);
 
