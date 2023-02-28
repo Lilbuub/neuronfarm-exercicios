887545aea252025317b00391819d7715c6d9bf87
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-28 19:31:44 -0300

    refeactor: correção do exercicio booleano (false)

--- 04-javascript-fundamentos/tipos_javascript_boolean/exercicio-1.js
+++ 04-javascript-fundamentos/tipos_javascript_boolean/exercicio-1.js
@@ -3,6 +3,6 @@ verdadeiro se você tiver mais de 18 anos, e falso caso o contrário.
 Em seguida imprima o valor na tela */
 
 let idade = 25;
-let idadeMaior18 = idade >= !!18;
-console.log = (idadeMaior18);
+let idadeMaior18 = idade >= 18;
+console.log(!idadeMaior18);
 
