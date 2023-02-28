248337f6a72d1325497b68e792d05d51b37ffd32
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-28 19:55:44 -0300

    feat: Exercicio2 exercicio de array com sort para ordenar

---
+++ 04-javascript-fundamentos/tipos_javascript_array/exercicio-2.js
@@ -0,0 +1,5 @@
+//Exercício 2: Crie um array com o nome "numeros" e atribua a ele pelo menos 5 números. Em seguida, utilize a função sort para ordenar o array e imprima o resultado na tela.
+
+const numeros = [10, 7, 5, 4, 2];
+numeros.sort();
+console.log(numeros); //10 vem antes do 2, pq em ponto de codigo unicode 10 vem antes.
