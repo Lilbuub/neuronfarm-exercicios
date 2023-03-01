eb69e36d3eb22823e4ae944c563d334ed9c94abd
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-02-28 21:31:24 -0300

    feat: Exericio2 de objeto inserção de outros atributos

---
+++ 04-javascript-fundamentos/tipos_javascript_object/exercicio2.js
@@ -0,0 +1,9 @@
+/* Exercício 2: Crie um objeto com o nome "livro" e atribua a ele as propriedades "título", "autor" e "páginas". 
+Em seguida, adicione uma nova propriedade "anoPublicacao" ao objeto e imprima todas as propriedades do objeto na tela.*/
+
+const livro = {}
+livro.titulo = 'As vantagens de ser invisivel';
+livro.autor = 'Fulano de tal';
+livro.paginas = 678;
+livro.anoPublicacao = 2023;
+console.log(livro);
