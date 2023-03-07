129d30cfd5e7d7c5512c15e1be66b222083308c5
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-03-07 18:40:29 -0300

    feat: exercicio2 null e undefined

---
+++ 04-javascript-fundamentos/null_e_undefined/exercicio-2.js
@@ -0,0 +1,7 @@
+//Exercício 2: Crie uma variável com o nome "numero" e atribua o valor null. Em seguida, verifique se a variável é igual a null e imprima o resultado na tela.
+
+let numero; //está vazio e sem valor.
+console.log(numero);
+
+numero = null; // atribui o valor null pra ele
+console.log(numero); 
