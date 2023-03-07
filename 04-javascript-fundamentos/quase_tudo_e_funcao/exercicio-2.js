2df39c7883bba060bf9718828d0e1803c94452e8
Lilbuub <thaisgiovanna.tg@gmail.com>
2023-03-07 19:49:32 -0300

    feat: exercicio2 função concatenar strings

---
+++ 04-javascript-fundamentos/quase_tudo_e_funcao/exercicio-2.js
@@ -0,0 +1,7 @@
+// Exercício 2: Crie uma função com o nome "concatena" que receba duas strings como parâmetros e retorne a concatenação das duas strings. Em seguida, chame a função e imprima o resultado na tela.
+
+let concatena = {
+    primeironome: 'Cachorro',
+    segundonome: '-Quente'
+}
+console.log(concatena.primeironome + concatena.segundonome);
